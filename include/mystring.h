d1102fe48e6f81851af8362049f44179ec16ba46
99003597 <78539729+99003597@users.noreply.github.com>
2021-03-02 09:54:24 +0530

    Add files via upload

---
+++ include/mystring.h
@@ -0,0 +1,10 @@
+#ifndef __MYSTRING__H
+#define __MYSTRING__H
+
+int mystrlen;
+int mystrcpy;
+int mystrcat;
+int mystrcmp;
+
+
+#endif
---
+++ include/myutils.h
@@ -0,0 +1,10 @@
+#ifndef __MYUTILS__H
+#define __MYUTILS__H
+
+int factorial;
+int isPrime;
+int isPalindrome;
+int vsum;
+
+
+#endif
