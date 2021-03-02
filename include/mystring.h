d3bd050d027cf99bda66086e6af4fd63765807dc
99003597 <78539729+99003597@users.noreply.github.com>
2021-03-02 10:08:20 +0530

    Add files via upload

---
+++ include/mystring.h
@@ -0,0 +1,10 @@
+#ifndef __MYSTRING__H
+#define __MYSTRING__H
+
+size_t mystrlen(const char *str);
+char *mystrcpy(char *str1, char *str2);
+char  *mystrcat(char *str1, char *str2,int n);
+int  mystrcmp(const char *str1, const char *str2);
+
+
+#endif
