22c6d96dffa43168b53d8e26c5ce2b46f6b77a07
99003597 <78539729+99003597@users.noreply.github.com>
2021-03-02 09:55:44 +0530

    Add files via upload

---
+++ src/bitmask.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "bitmask.h"
+
+int set()
+{
+	
+	
+	
+	
+	
+	
+}i
+nt reset()
+{
+	
+	
+	
+	
+	
+	
+}
+int flip()
+{
+	
+	
+	
+	
+	
+}
+int query()
+{
+	
+	
+	
+	
+	
+	
+}
---
+++ src/myutils.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "myutils.h"
+
+
+
+
+
+int factorial(unsigned int x);
+int isPrime(int);
+int isPalindrome(char *str);
+int vsum(int x, ...);
