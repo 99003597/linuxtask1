f438523a1451bcd51ed4404be7e6e61be68fb0a2
99003597 <vikramanandnarayan.hegde@ltts.com>
2021-03-04 14:45:29 +0530

    h

--- task1/problem2/prob2.c
+++ task1/problem2/prob2.c
@@ -4,13 +4,14 @@
 int main()
 {
     FILE * file;
+    int space;
     char path[100];
     char ch;
     int character, word, line;
     //printf("Enter source file path: ");
     //scanf("%s", path);
-    file = fopen("/home/ltts/linuxtask1/task2/calls/abcd.txt", "r");
-    if (file == NULL)
+    file = fopen("nd.txt", "r");
+   if (file == NULL)
     {
         printf("\nUnable to open file.\n");
         printf("Please check if file exists and you have read privilege.\n");
@@ -30,12 +31,17 @@ int main()
         /* Check words */
         if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
             word++;
+
+
+        if(ch == ' ')
+                space++;
     }
     
     printf("\n");
     printf("Total characters = %d\n", character);
     printf("Total lines      = %d\n", line);
-
+ printf("Total spaces = %d\n", space);
+   
     fclose(file);
 
     return 0;
