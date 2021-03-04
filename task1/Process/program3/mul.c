85fbfa175e46c01495462d17c46bf6665013ae5e
99003597 <vikramanandnarayan.hegde@ltts.com>
2021-03-04 15:55:44 +0530

    h

---
+++ task1/Process/program3/mul.c
@@ -0,0 +1,4 @@
+#include "func.h"
+int mul(int x, int y) {
+  return x * y;
+}
---
+++ task1/Process/program3/testt.c
@@ -0,0 +1,10 @@
+#include "func.h"
+#include <stdio.h>
+
+int main1() {
+  int a, b, c, d;
+  a = 10;
+  b = 20;
+  c = mul(a, b);
+  return c;
+}
